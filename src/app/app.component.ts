03cb62f2245781cdd3659b0abc82c9b5b424d44b
El Mahdi Id Lahcen <idlahcen.elmahdi@etu.uae.ac.ma>
2024-10-22 19:43:09 +0100

    front & back relation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EmployeeListComponent ],
+  imports: [RouterOutlet, EmployeeListComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { NgFor } from '@angular/common';
+import { EmployeeService } from '../employee.service';
+import { provideHttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-employee-list',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './employee-list.component.html',
   styleUrl: './employee-list.component.css'
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
 
+  employees?: Employee[];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+  private getEmployees() {
+    this.employeeService.getEmployeesList().subscribe(data => {
+      this.employees = data;
+    });
+  }
 }
